5127de75b58b2eeeed1bf285da1900d6e6407700
Gary Sibanda <sibandamthandazo@gmail.com>
2024-08-11 00:13:23 -0600

    Added dynamic padding to Logo

--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -47,9 +47,9 @@ export const NavBar = () => {
           </Nav>
           <span className='navbar-text'>
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt ="" /></a>
-              <a href="#"><img src={navIcon2} alt ="" /></a>
-              <a href="#"><img src={navIcon3} alt ="" /></a>
+              <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer"><img src={navIcon1} alt ="" /></a>
+              <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><img src={navIcon2} alt ="" /></a>
+              <a href="https://www.instagram.com/" target="_blank" rel="noreferrer"><img src={navIcon3} alt ="" /></a>
             </div>
             <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
           </span>
