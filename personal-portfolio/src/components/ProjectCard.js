19514cca00001daf30d4a9ce76c8bda04754cab9
Gary Sibanda <sibandamthandazo@gmail.com>
2024-08-11 19:31:29 -0600

    Hover effect enlarge on cards

--- personal-portfolio/src/components/ProjectCard.js
+++ personal-portfolio/src/components/ProjectCard.js
@@ -1,15 +1,25 @@
 import { Col } from "react-bootstrap";
+import { useState } from "react";
 
 export const ProjectCard = ({ title, description, imgUrl }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);
+
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} alt={"url"}/>
+      <div
+        className={`proj-imgbx ${isHovered ? "proj-imgbx-hover" : ""}`}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <img src={imgUrl} alt={"url"} />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
         </div>
       </div>
     </Col>
-  )
-}
+  );
+};
